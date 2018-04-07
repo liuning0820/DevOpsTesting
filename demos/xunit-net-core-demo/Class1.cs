95aa03e9cc3746eae20b62434a538c896d3425f1
liuning0820 <liuning0820@outlook.com>
2018-04-07 11:59:46 +0800

    ignore the fail test.

--- demos/xunit-net-core-demo/Class1.cs
+++ demos/xunit-net-core-demo/Class1.cs
@@ -9,11 +9,11 @@ namespace xunit_net_core_demo
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+        // [Fact]
+        // public void FailingTest()
+        // {
+        //     Assert.Equal(5, Add(2, 2));
+        // }
 
         int Add(int x, int y)
         {
